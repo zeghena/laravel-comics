536595338090f2bbcc83d660c3e81fac42cbebf1
davidemarchini <marchini73@gmail.com>
2024-03-16 17:20:26 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,18 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+    },
     plugins: [
         laravel({
-            input: ["resources/scss/app.scss", "resources/js/app.js"],
+            input: [
+                "resources/scss/app.scss",
+                "resources/js/app.js",
+                "resources/js/bootstrap.js",
+            ],
             refresh: true,
         }),
     ],
